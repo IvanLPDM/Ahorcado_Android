5fa3db484d6579df619157df22159ac7dc6562e3
Ivan <ivan.leris@enti.cat>
2024-11-18 20:38:34 +0100

    Probando cosas

--- app/src/main/java/com/example/ahorcado/MainActivity.kt
+++ app/src/main/java/com/example/ahorcado/MainActivity.kt
@@ -5,7 +5,13 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        //Thread.sleep(2000);
+        //Seteamos el estilo de la aplicacion, para no poner el del splash screen
+        //setTheme(R.style.AppTheme)
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
+
